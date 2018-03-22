307df1f6a87afdf59db76ddf257fff51ced39cf6
mattmorgan6 <mattmorgan6@gmail.com>
2018-03-22 12:30:44 -0700

    started mainActivity design

---
+++ app/src/main/java/com/example/mattm/calendar/AddClassActivity.java
@@ -0,0 +1,19 @@
+package com.example.mattm.calendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_class);
+
+
+        //this is where you can add a class and period to your schedule
+
+
+
+    }
+}
