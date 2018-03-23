4014333a0506e252b2321a0dfda5fe03b9711ef2
SunnySoldier357 <sandeep01@hotmail.sg>
2018-03-22 17:36:30 -0700

    added button click event handler

--- app/src/main/java/com/example/mattm/calendar/AddClassActivity.java
+++ app/src/main/java/com/example/mattm/calendar/AddClassActivity.java
@@ -9,11 +9,7 @@ public class AddClassActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_class);
-
-
-        //this is where you can add a class and period to your schedule
-
-
-
+        
+        // TODO: Add a class and period to schedule
     }
 }
--- app/src/main/java/com/example/mattm/calendar/MainActivity.java
+++ app/src/main/java/com/example/mattm/calendar/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mattm.calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // DEBUG PURPOSES
         Toast.makeText(this, "hello KMS friends", Toast.LENGTH_SHORT).show();
+    }
+    
+    public void addClassButton_Clicked(View view)
+    {
+        // TODO: Switch to Add Class Activity
     }
 }
