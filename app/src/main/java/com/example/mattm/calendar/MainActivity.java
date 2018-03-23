790dbe43f886fda67130e83413f2e580ebe9ad0b
mattmorgan6 <mattmorgan6@gmail.com>
2018-03-22 20:04:52 -0700

    change in how design works

--- app/src/main/java/com/example/mattm/calendar/MainActivity.java
+++ app/src/main/java/com/example/mattm/calendar/MainActivity.java
@@ -4,19 +4,36 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String[] periods = {"King", "Fowler", "Reinsch"};
+
+
+        //for sample purposes
+        ListView periodsList = (ListView)findViewById(R.id.periodsList);
+
+
         // DEBUG PURPOSES
         Toast.makeText(this, "hello KMS friends", Toast.LENGTH_SHORT).show();
     }
-    
+
+
+
     public void addClassButton_Clicked(View view) {
         Intent intent = new Intent(this, AddClassActivity.class);
         startActivity(intent);
