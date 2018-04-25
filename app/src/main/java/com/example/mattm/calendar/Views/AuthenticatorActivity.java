ae81b5e6bf5545469ef3afd540578f615fec7309
mattmorgan6 <mattmorgan6@gmail.com>
2018-04-24 17:43:03 -0700

    some ui coloring

--- app/src/main/java/com/example/mattm/calendar/Views/AuthenticatorActivity.java
+++ app/src/main/java/com/example/mattm/calendar/Views/AuthenticatorActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.amazonaws.mobile.auth.core.IdentityManager;
 import com.amazonaws.mobile.auth.core.SignInStateChangeListener;
