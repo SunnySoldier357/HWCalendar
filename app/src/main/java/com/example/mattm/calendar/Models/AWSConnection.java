73edcd6166e320e60b959af0b6c566f3cf65cb0d
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2018-04-25 17:34:50 -0700

    More templating for AWSConnection class (AWS Logic Class)

--- app/src/main/java/com/example/mattm/calendar/Models/AWSConnection.java
+++ app/src/main/java/com/example/mattm/calendar/Models/AWSConnection.java
@@ -1,6 +1,41 @@
 package com.example.mattm.calendar.Models;
 
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
 public class AWSConnection
 {
     // TODO: Put all AWS logic in this class
+    
+    //Public Methods
+    public Assignment AddAssignment()
+    {
+        // TODO: Add an assignment to the DB
+        return null;
+    }
+    
+    public Subject AddSubject()
+    {
+        // TODO: Add a subject to the DB
+        return null;
+    }
+    
+    public User AddUser()
+    {
+        // TODO: Add a User to the DB
+        return null;
+    }
+    
+    public ArrayAdapter<User> GetUsers()
+    {
+        // TODO: Return a list of Users from the DB
+        return null;
+    }
+    
+    // Private Methods
+    private void initialise()
+    {
+        // TODO: Initialise database
+    }
 }
