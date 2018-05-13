863037295248dfc6fd985f48ff05471088646af4
mattmorgan6 <mattmorgan6@gmail.com>
2018-05-12 19:06:21 -0700

    hamburger menu works with everything!!!

--- app/src/main/java/com/example/mattm/calendar/Views/MainActivity.java
+++ app/src/main/java/com/example/mattm/calendar/Views/MainActivity.java
@@ -2,9 +2,18 @@ package com.example.mattm.calendar.Views;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -21,8 +30,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     // Public Properties
     public ArrayAdapter<String> periodsAdapter;
     public ArrayAdapter<String> eventsAdapter;
@@ -37,6 +45,39 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
+
+
+
+
+
+
+
+        //----------------------------------------------------------------------------------------------------------------------
 
         // Initialising database stuff
         AWSConnection awsConnection = null;
@@ -86,16 +127,22 @@ public class MainActivity extends AppCompatActivity
     }
     
     // Event Handlers
-    public void addClassButton_Clicked(View view)
-    {
-        Intent intent = new Intent(this, AddSubjectActivity.class);
-        startActivity(intent);
-    }
-    
+
+
+
+
     public void classItem_Clicked(View view, int position)
     {
         Intent intent = new Intent(this,AddEventActivity.class);
         intent.putExtra("ClassName", periodsAdapter.getItem(position));
+        startActivity(intent);
+    }
+
+    /*
+
+    public void addClassButton_Clicked(View view)
+    {
+        Intent intent = new Intent(this, AddSubjectActivity.class);
         startActivity(intent);
     }
     
@@ -118,7 +165,7 @@ public class MainActivity extends AppCompatActivity
     {
         Intent intent = new Intent(this, AuthenticatorActivity.class);
         startActivity(intent);
-    }
+    }   */
     
     // Public Methods
     public void GetCalendarDay()
@@ -140,5 +187,76 @@ public class MainActivity extends AppCompatActivity
                 Toast.makeText(MainActivity.this, d.toString(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+
+    //---------------------------------------------------------------------------------------------------------------------
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        //goes to the login screen
+        if (id == R.id.nav_camera) {
+            Intent intent = new Intent(this, AuthenticatorActivity.class);
+            startActivity(intent);
+            //doesn't do anything, in the future will be used to logout
+        } else if (id == R.id.nav_gallery) {
+            Toast.makeText(this, "Logged Out", Toast.LENGTH_SHORT).show();
+            //goes to add a class activity
+        } else if (id == R.id.nav_manage) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void classAddButton_Clicked(View view) {
+        Intent intent = new Intent(this, AddSubjectActivity.class);
+        //intent.putExtra("ID", ID);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
