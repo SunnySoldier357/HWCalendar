586e92aed2f4279b05d5b81ae57adb67f5754672
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2018-05-26 07:41:59 -0700

    Clean up settings page and xml

--- app/src/main/java/com/example/mattm/calendar/Views/SettingsActivity.java
+++ app/src/main/java/com/example/mattm/calendar/Views/SettingsActivity.java
@@ -7,15 +7,18 @@ import android.widget.Toast;
 
 import com.example.mattm.calendar.R;
 
-public class SettingsActivity extends AppCompatActivity {
-
+public class SettingsActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
     }
 
-    public void aboutOnClick(View view) {
-        Toast.makeText(this, "\n developed by KMS \n", Toast.LENGTH_SHORT).show();
+    // Event Handlers
+    public void aboutOnClick(View view)
+    {
+        Toast.makeText(this, "\n Developed by KMS \n", Toast.LENGTH_SHORT).show();
     }
 }
