3183e17fe1f789175ecc0e927be59614e19999f2
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2018-06-06 20:03:28 -0700

    Minimal cleaning

--- app/src/main/java/com/example/mattm/calendar/Views/SplashActivity.java
+++ app/src/main/java/com/example/mattm/calendar/Views/SplashActivity.java
@@ -5,14 +5,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by morgmat19 on 6/6/2018.
- */
-
-public class SplashActivity extends AppCompatActivity {
-
+public class SplashActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         Intent intent = new Intent(this, AuthenticatorActivity.class);
