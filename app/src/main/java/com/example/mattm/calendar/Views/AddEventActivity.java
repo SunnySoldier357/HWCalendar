e56264857948002d6112b65485c94c934c31baa7
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2018-06-08 17:57:43 -0700

    Clean part 1

--- app/src/main/java/com/example/mattm/calendar/Views/AddEventActivity.java
+++ app/src/main/java/com/example/mattm/calendar/Views/AddEventActivity.java
@@ -1,6 +1,8 @@
 package com.example.mattm.calendar.Views;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -8,10 +10,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,19 +19,19 @@ import com.example.mattm.calendar.Models.AWSConnection;
 import com.example.mattm.calendar.R;
 
 import java.util.Calendar;
-import java.util.Date;
 
 public class AddEventActivity extends AppCompatActivity
 {
     // Private Properties
     private AWSConnection awsConnection;
-
-    private TextView mDisplayDate;
-    private DatePickerDialog.OnDateSetListener mDateSetListener;
-
-    private String year;
-    private String month;
+    
+    private OnDateSetListener mDateSetListener;
+    
     private String day;
+    private String month;
+    private String year;
+    
+    private TextView mDisplayDate;
     
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -76,62 +76,61 @@ public class AddEventActivity extends AppCompatActivity
     }
     
     // Private Methods
-    private void initializeDate() {
-        final Calendar c = Calendar.getInstance();
-        year = String.valueOf(c.YEAR);
-        month = String.valueOf(c.MONTH + 1);
-        day = String.valueOf(c.DAY_OF_MONTH);
+    private void initializeDate()
+    {
+        year = String.valueOf(Calendar.YEAR);
+        month = String.valueOf(Calendar.MONTH + 1);
+        day = String.valueOf(Calendar.DAY_OF_MONTH);
     }
 
-    private void setUpDatePicker() {
+    private void setUpDatePicker()
+    {
+        mDisplayDate.setOnClickListener(view ->
+        {
+            Calendar cal = Calendar.getInstance();
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH);
+            int day = cal.get(Calendar.DAY_OF_MONTH);
 
-        mDisplayDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog dialog = new DatePickerDialog(
+                    AddEventActivity.this,
+                    android.R.style.Theme_Holo,     // Theme can be adjusted & HoloDialog is full screen
+                    mDateSetListener,
+                    year, month, day);
+            
+            // Sets the background transparent (unnecessary)
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-                DatePickerDialog dialog = new DatePickerDialog(
-                        AddEventActivity.this,
-                        android.R.style.Theme_Holo,     //theme can be adjusted     //holoDialog is full screen
-                        mDateSetListener,
-                        year, month, day);
-                //sets the background transparent (unecessary)
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-
-                dialog.show();
-            }
+            dialog.show();
         });
 
-        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int i_year, int i_month, int i_day) {    //the month starts at 0
-                i_month++;
-                year = String.valueOf(i_year);
-                month = String.valueOf(i_month);
-                day = String.valueOf(i_day);
-                showDate();
-            }
+        mDateSetListener = (datePicker, i_year, i_month, i_day) ->
+        {
+            // The month starts at 0
+            i_month++;
+            year = String.valueOf(i_year);
+            month = String.valueOf(i_month);
+            day = String.valueOf(i_day);
+            showDate();
         };
     }
 
-    private void showDate() {
+    private void showDate()
+    {
         mDisplayDate = findViewById(R.id.tvDate);
-        String date = month + "/" + day + "/" + year;
+        String date = String.format("%s/%s/%s", month, day, year);
         mDisplayDate.setText(date);
     }
 
-
-     //Accessors
+    // Accessors
     public String GetDay()
     {
         return day;
     }
 
-    public String GetMonth()        //should return integer for month
+    public String GetMonth()
     {
+        // Should return integer for month
         return month;
     }
 
@@ -149,5 +148,4 @@ public class AddEventActivity extends AppCompatActivity
     {
         return ((EditText) findViewById(R.id.eventName)).getText().toString();
     }
-
 }
